9320b0b645a0ad74bc59e9a597e09375ca0bca1b
Anderson Banihirwe <axbanihirwe@ualr.edu>
2021-08-24 10:28:44 -0600

    Remove initial color mode

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,6 +1,5 @@
 import NextDocument, { Html, Head, Main, NextScript } from "next/document"
 import { ColorModeScript } from "@chakra-ui/react"
-import theme from "../theme"
 export default class Document extends NextDocument {
   render() {
     return (
@@ -8,7 +7,7 @@ export default class Document extends NextDocument {
         <Head />
         <body>
           {/* Make Color mode to persists when you refresh the page. */}
-          <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+          <ColorModeScript />
           <Main />
           <NextScript />
         </body>
--- src/theme.js
+++ src/theme.js
@@ -1,7 +1,6 @@
 import { extendTheme } from "@chakra-ui/react"
 
 const config = {
-  initialColorMode: "dark",
   useSystemColorMode: true,
 }
 
