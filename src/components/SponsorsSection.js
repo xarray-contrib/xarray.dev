13c662300ea3b185f1a5866ffa9090e0bd2a3963
Anderson Banihirwe <axbanihirwe@ualr.edu>
2021-09-06 22:06:52 -0600

    Reduce logo sizes

--- src/components/SponsorsSection.js
+++ src/components/SponsorsSection.js
@@ -48,7 +48,7 @@ export const SponsorsSection = () => {
               rounded={"full"}
               mb={1}
             >
-              <Image w={60} h={60} src={sponsor.logo} alt={sponsor.name} />
+              <Image w={32} h={32} src={sponsor.logo} alt={sponsor.name} />
             </Flex>
           ))}
         </Stack>
