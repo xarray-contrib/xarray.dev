eb9949d021d38a067f18d091e66535acd1a8e77a
Anderson Banihirwe <axbanihirwe@ualr.edu>
2021-09-28 22:36:20 -0600

    Use all data + style the page

--- src/components/Charts.js
+++ src/components/Charts.js
@@ -1,84 +1,89 @@
 import React from "react"
 import { Line } from "react-chartjs-2"
+import { Container } from "@chakra-ui/react"
+import useSWR from "swr"
 
-const times = [
-  1611017460, 1611304380, 1612484160, 1614154440, 1614240480, 1615133700,
-  1615565700, 1616026620, 1618128120, 1618214880, 1619050080, 1619280240,
-  1620202080, 1622882160, 1624926360, 1627172820, 1627546260, 1629592020,
-  1630685580, 1632586260,
-].map((x) => new Date(x * 1000).toLocaleDateString())
+const fetcher = (...args) => fetch(...args).then((res) => res.json())
 
-const issuesData = {
-  labels: times,
-  datasets: [
-    {
-      label: "# of open issues",
-      data: [
-        709, 688, 708, 690, 682, 709, 710, 742, 693, 773, 696, 694, 744, 729,
-        692, 778, 705, 680, 739, 764,
-      ],
-      fill: false,
-      backgroundColor: "rgb(255, 99, 132)",
-      borderColor: "rgba(255, 99, 132, 0.2)",
-    },
-  ],
-}
+export const Charts = () => {
+  const { data, error } = useSWR(
+    "https://gist.githubusercontent.com/andersy005/4f62b7b0cbf943b158cb11068e80c7c8/raw/6006f3e688e881d944b7b22be043fc4ed8ba5c18/xarray-repo-data.json",
+    fetcher
+  )
 
-const pullsData = {
-  labels: times,
-  datasets: [
-    {
-      label: "# of open pull requests",
-      data: [
-        108, 99, 108, 101, 101, 109, 108, 99, 101, 112, 104, 111, 102, 100, 108,
-        116, 105, 101, 94, 110,
-      ],
-      fill: false,
-      backgroundColor: "rgb(54, 162, 235)",
-      borderColor: "rgba(54, 162, 235, 0.2)",
-    },
-  ],
-}
+  const pulls = data.map((item) => item.open_pull_requests)
+  const issues = data.map((item) => item.open_issues)
+  const dates = data.map((item) =>
+    new Date(item.timestamp * 1000).toLocaleDateString()
+  )
 
-const options = {
-  type: "line",
-  data: issuesData,
-  options: {
-    responsive: true,
-    interaction: {
-      mode: "nearest",
-      axis: "x",
-      intersect: false,
-    },
-    plugins: {
-      decimation: { enabled: true, algorithm: "lttb", samples: 50 },
-      legend: {
-        position: "top",
+  if (error) return <div>failed to load</div>
+  if (!data) return <div>loading...</div>
+
+  const issuesData = {
+    labels: dates,
+    datasets: [
+      {
+        label: "# of open issues",
+        data: issues,
+        fill: false,
+        backgroundColor: "rgb(255, 99, 132)",
+        borderColor: "rgba(255, 99, 132, 0.2)",
       },
-      title: {
-        display: true,
-        text: "",
+    ],
+  }
+
+  const pullsData = {
+    labels: dates,
+    datasets: [
+      {
+        label: "# of open pull requests",
+        data: pulls,
+        fill: false,
+        backgroundColor: "rgb(54, 162, 235)",
+        borderColor: "rgba(54, 162, 235, 0.2)",
       },
-    },
-    scales: {
-      x: {
-        type: "time",
-        ticks: {
-          source: "auto",
-          // Disabled rotation for performance
-          maxRotation: 0,
-          autoSkip: true,
+    ],
+  }
+
+  const options = {
+    type: "line",
+    data: issuesData,
+    options: {
+      responsive: true,
+      interaction: {
+        mode: "nearest",
+        axis: "x",
+        intersect: false,
+      },
+      plugins: {
+        decimation: { enabled: true, algorithm: "lttb", samples: 100 },
+        legend: {
+          position: "top",
+        },
+        title: {
+          display: true,
+          text: "",
+        },
+      },
+      scales: {
+        x: {
+          type: "time",
+          ticks: {
+            source: "auto",
+            // Disabled rotation for performance
+            maxRotation: 0,
+            autoSkip: true,
+          },
         },
       },
     },
-  },
-}
+  }
 
-export const Charts = () => {
   return (
-    <>
+    <Container maxW={"6xl"} mt={10} p={16}>
       <Line data={issuesData} options={options} />
       <Line data={pullsData} options={options} />
-    </>
+    </Container>
   )
 }
