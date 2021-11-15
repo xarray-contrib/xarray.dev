4956d31acd1f05082f07d6e0f8e0400454c220f4
Anderson Banihirwe <axbanihirwe@ualr.edu>
2021-11-15 12:33:34 -0700

    Use stat with indicator (#73)

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -8,6 +8,9 @@ import {
   Stat,
   StatLabel,
   StatNumber,
+  StatGroup,
+  StatHelpText,
+  StatArrow,
   useColorModeValue,
   Spinner,
 } from "@chakra-ui/react"
@@ -20,36 +23,56 @@ import * as d3 from "d3"
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json())
 
-const StatisticsCard = ({ title, stat, icon, includeDiff = false }) => {
+const StatisticsCard = ({ title, stat, icon, diff = undefined }) => {
+  let diffElement
+  if (diff) {
+    let color
+    if (diff.type === "increase") {
+      color = "red.500"
+    } else {
+      color = "green.500"
+    }
+    diffElement = (
+      <StatHelpText>
+        <StatArrow type={diff.type} color={color} />
+        {diff.value}
+      </StatHelpText>
+    )
+  } else {
+    diffElement = <Text>{""}</Text>
+  }
   return (
-    <Stat
-      px={{ base: 2, md: 4 }}
-      py={"5"}
-      shadow={"xl"}
-      border={"1px solid"}
-      borderColor={useColorModeValue("gray.800", "gray.500")}
-      rounded={"lg"}
-    >
-      <Flex justifyContent={"space-between"}>
-        <Box pl={{ base: 2, md: 4 }}>
-          <StatLabel fontWeight={"medium"}>{title}</StatLabel>
-          <StatNumber fontSize={"2xl"} fontWeight={"medium"}>
-            {stat}
-          </StatNumber>
-        </Box>
-        <Box
-          my={"auto"}
-          color={useColorModeValue("gray.800", "gray.200")}
-          alignContent={"center"}
-        >
-          {icon}
-        </Box>
-      </Flex>
-    </Stat>
+    <StatGroup>
+      <Stat
+        px={{ base: 2, md: 4 }}
+        py={"5"}
+        shadow={"xl"}
+        border={"1px solid"}
+        borderColor={useColorModeValue("gray.800", "gray.500")}
+        rounded={"lg"}
+      >
+        <Flex justifyContent={"space-between"}>
+          <Box pl={{ base: 2, md: 4 }}>
+            <StatLabel fontWeight={"medium"}>{title}</StatLabel>
+            <StatNumber fontSize={"2xl"} fontWeight={"medium"}>
+              {stat}
+            </StatNumber>
+            {diffElement}
+          </Box>
+          <Box
+            my={"auto"}
+            color={useColorModeValue("gray.800", "gray.200")}
+            alignContent={"center"}
+          >
+            {icon}
+          </Box>
+        </Flex>
+      </Stat>
+    </StatGroup>
   )
 }
 
-const DatasetteResult = ({ query }) => {
+const DatasetteStatsCard = ({ query, title, icon }) => {
   const { data, error } = useSWR(query, fetcher)
   if (error) return <Text>failed to load</Text>
   if (!data)
@@ -62,10 +85,10 @@ const DatasetteResult = ({ query }) => {
         size="xl"
       />
     )
-  return data[0].total_rows
+  return <StatisticsCard stat={data[0].total_rows} title={title} icon={icon} />
 }
 
-const TimeseriesAggResult = ({ query }) => {
+const TimeseriesAggStatsCard = ({ query, title, icon }) => {
   const { data, error } = useSWR(query, fetcher)
   if (error) return <Text>failed to load</Text>
   if (!data)
@@ -99,27 +122,24 @@ const TimeseriesAggResult = ({ query }) => {
 
   const result = d3.median(data, (d) => d.age_in_days)
   const previousMonthResult = d3.median(previousMonthData, (d) => d.age_in_days)
-  const diff = result - previousMonthResult
   const diffPercentage = ((previousMonthResult - result) / result) * 100
-  const diffPercentageFormatted = (value) => {
-    const formatted = d3.format(".2f")(Math.abs(value))
-    return value < 0 ? `${formatted}% ↑` : `${formatted}% ↓`
-  }
-  console.log(
-    result,
-    previousMonthResult,
-    diff,
-    diffPercentage,
-    diffPercentageFormatted(diffPercentage)
-  )
 
-  return result <= 2
-    ? `${d3.format(".1f")(result * 24)} hours (${diffPercentageFormatted(
-        diffPercentage
-      )})`
-    : `${d3.format(".1f")(result)} days (${diffPercentageFormatted(
-        diffPercentage
-      )})`
+  const change = {
+    type: diffPercentage < 0 ? "increase" : "decrease",
+    value: `${d3.format(".2f")(Math.abs(diffPercentage))}% since last month`,
+  }
+  return (
+    <StatisticsCard
+      title={title}
+      icon={icon}
+      stat={
+        result <= 2
+          ? `${d3.format(".1f")(result * 24)} hours`
+          : `${d3.format(".1f")(result)} days`
+      }
+      diff={change}
+    />
+  )
 }
 
 export const Statistics = () => {
@@ -140,52 +160,40 @@ export const Statistics = () => {
           stat={"15"}
           icon={<BsPerson size={"3em"} />}
         />
-        <StatisticsCard
+        <DatasetteStatsCard
           title={"Contributors"}
-          stat={
-            <DatasetteResult
-              query={
-                "https://pydata-datasette.herokuapp.com/xarray/_analyze_tables_/contributors,user_id.json?_shape=array"
-              }
-            />
+          query={
+            "https://pydata-datasette.herokuapp.com/xarray/_analyze_tables_/contributors,user_id.json?_shape=array"
           }
           icon={<BsPeople size={"3em"} />}
         />
-        <StatisticsCard
+
+        <DatasetteStatsCard
           title={"Stargazers"}
-          stat={
-            <DatasetteResult
-              query={
-                "https://pydata-datasette.herokuapp.com/xarray/_analyze_tables_/stars,user.json?_shape=array"
-              }
-            />
-          }
           icon={<GoStar size={"3em"} />}
+          query={
+            "https://pydata-datasette.herokuapp.com/xarray/_analyze_tables_/stars,user.json?_shape=array"
+          }
         />
-        <StatisticsCard
+
+        <DatasetteStatsCard
           title={"Dependent Projects"}
-          stat={
-            <DatasetteResult
-              query={
-                "https://pydata-datasette.herokuapp.com/xarray/_analyze_tables_/dependents,dependent.json?_shape=array"
-              }
-            />
+          query={
+            "https://pydata-datasette.herokuapp.com/xarray/_analyze_tables_/dependents,dependent.json?_shape=array"
           }
           icon={<GoPackage size={"3em"} />}
         />
+
         <StatisticsCard
           title={"Docs Views"}
           stat={"7,000"}
           icon={<GoBook size={"3em"} />}
         />
-        <StatisticsCard
+
+        <DatasetteStatsCard
           title={"Releases"}
-          stat={
-            <DatasetteResult
-              query={
-                "https://pydata-datasette.herokuapp.com/xarray/_analyze_tables_/releases,id.json?_shape=array"
-              }
-            />
+          query={
+            "https://pydata-datasette.herokuapp.com/xarray/_analyze_tables_/releases,id.json?_shape=array"
           }
           icon={<GoTag size={"3em"} />}
         />
@@ -203,27 +211,19 @@ export const Statistics = () => {
 
       <SimpleGrid columns={{ base: 1, md: 2 }} spacing={{ base: 5, lg: 8 }}>
         {" "}
-        <StatisticsCard
+        <TimeseriesAggStatsCard
           title={"Median time a pull request is open"}
-          stat={
-            <TimeseriesAggResult
-              query={
-                "https://pydata-datasette.herokuapp.com/xarray.json?_shape=array&&sql=select%0D%0A++id%2C%0D%0A++number%2C%0D%0A++state%2C%0D%0A++created_at%2C%0D%0A++closed_at%2C%0D%0A++julianday%28closed_at%29+-+julianday%28created_at%29+as+age_in_days%0D%0Afrom%0D%0A++issues+as+data%0D%0Awhere%0D%0A++type+%3D+%27pull%27%0D%0A++and+state+%3D+%27closed%27%0D%0Aorder+by%0D%0A++id"
-              }
-            />
+          query={
+            "https://pydata-datasette.herokuapp.com/xarray.json?_shape=array&&sql=select%0D%0A++id%2C%0D%0A++number%2C%0D%0A++state%2C%0D%0A++created_at%2C%0D%0A++closed_at%2C%0D%0A++julianday%28closed_at%29+-+julianday%28created_at%29+as+age_in_days%0D%0Afrom%0D%0A++issues+as+data%0D%0Awhere%0D%0A++type+%3D+%27pull%27%0D%0A++and+state+%3D+%27closed%27%0D%0Aorder+by%0D%0A++id"
           }
           icon={<GiDuration size={"3em"} />}
         />
-        <StatisticsCard
+        <TimeseriesAggStatsCard
           title={"Median time an issue is open"}
-          stat={
-            <TimeseriesAggResult
-              query={
-                "https://pydata-datasette.herokuapp.com/xarray.json?_shape=array&&sql=select%0D%0A++id%2C%0D%0A++number%2C%0D%0A++state%2C%0D%0A++created_at%2C%0D%0A++closed_at%2C%0D%0A++julianday%28closed_at%29+-+julianday%28created_at%29+as+age_in_days%0D%0Afrom%0D%0A++issues+as+data%0D%0Awhere%0D%0A++type+%3D+%27issue%27%0D%0A++and+state+%3D+%27closed%27%0D%0Aorder+by%0D%0A++id"
-              }
-            />
-          }
           icon={<GiDuration size={"3em"} />}
+          query={
+            "https://pydata-datasette.herokuapp.com/xarray.json?_shape=array&&sql=select%0D%0A++id%2C%0D%0A++number%2C%0D%0A++state%2C%0D%0A++created_at%2C%0D%0A++closed_at%2C%0D%0A++julianday%28closed_at%29+-+julianday%28created_at%29+as+age_in_days%0D%0Afrom%0D%0A++issues+as+data%0D%0Awhere%0D%0A++type+%3D+%27issue%27%0D%0A++and+state+%3D+%27closed%27%0D%0Aorder+by%0D%0A++id"
+          }
         />
       </SimpleGrid>
     </Box>
