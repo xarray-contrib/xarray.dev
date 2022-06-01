59424bb9f8f37d8f2191aa19646fc99c95ac4fba
Anderson Banihirwe <axbanihirwe@ualr.edu>
2022-06-01 16:27:54 -0600

    fix typo

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,6 +1,7 @@
 import NextDocument, { Html, Head, Main, NextScript } from "next/document"
 
 import { ColorModeScript } from "@chakra-ui/react"
+import { GTM_ID } from "../lib/ga"
 export default class Document extends NextDocument {
   render() {
     return (
