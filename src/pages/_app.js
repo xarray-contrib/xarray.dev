281185e24a086b38720f31e50119cf7689e46e4e
Anderson Banihirwe <axbanihirwe@ualr.edu>
2022-06-01 16:48:55 -0600

    replace `google-tag-manager` with `google-analytics-tag` (#219)

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,14 +4,19 @@ import { useRouter } from "next/router"
 import Script from "next/script"
 import theme from "../theme"
 import { MDXComponentsProvider } from "components/mdx"
-import { GTM_ID, pageview } from "../lib/ga"
+import * as gtag from "../lib/ga"
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter()
   useEffect(() => {
-    router.events.on("routeChangeComplete", pageview)
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on("routeChangeComplete", handleRouteChange)
+    router.events.on("hashChangeComplete", handleRouteChange)
     return () => {
-      router.events.off("routeChangeComplete", pageview)
+      router.events.off("routeChangeComplete", handleRouteChange)
+      router.events.off("hashChangeComplete", handleRouteChange)
     }
   }, [router.events])
 
@@ -19,15 +24,20 @@ function MyApp({ Component, pageProps }) {
     <ChakraProvider resetCSS theme={theme}>
       {/* Google Tag Manager - Global base code */}
       <Script
-        id="gtag-base"
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer', '${GTM_ID}');
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
           `,
         }}
       />
