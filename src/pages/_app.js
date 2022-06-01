761876d20305b7838844f161891f64df37f0bb8c
Anderson Banihirwe <axbanihirwe@ualr.edu>
2022-06-01 16:29:12 -0600

    Fix client side rendering (#218)

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,30 +1,36 @@
 import { ChakraProvider } from "@chakra-ui/react"
 import { useEffect } from "react"
 import { useRouter } from "next/router"
+import Script from "next/script"
 import theme from "../theme"
 import { MDXComponentsProvider } from "components/mdx"
-import * as ga from "../lib/ga"
+import { GTM_ID, pageview } from "../lib/ga"
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter()
-
   useEffect(() => {
-    const handleRouteChange = (url) => {
-      ga.pageview(url)
-    }
-    //When the component is mounted, subscribe to router changes
-    //and log those page views
-    router.events.on("routeChangeComplete", handleRouteChange)
-
-    // If the component is unmounted, unsubscribe
-    // from the event with the `off` method
+    router.events.on("routeChangeComplete", pageview)
     return () => {
-      router.events.off("routeChangeComplete", handleRouteChange)
+      router.events.off("routeChangeComplete", pageview)
     }
   }, [router.events])
 
   return (
     <ChakraProvider resetCSS theme={theme}>
+      {/* Google Tag Manager - Global base code */}
+      <Script
+        id="gtag-base"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer', '${GTM_ID}');
+          `,
+        }}
+      />
       <MDXComponentsProvider>
         <Component {...pageProps} />
       </MDXComponentsProvider>
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,5 +1,7 @@
 import NextDocument, { Html, Head, Main, NextScript } from "next/document"
+
 import { ColorModeScript } from "@chakra-ui/react"
+import { GTM_ID } from "../lib/ga"
 export default class Document extends NextDocument {
   render() {
     return (
@@ -17,25 +19,16 @@ export default class Document extends NextDocument {
             type="text/css"
             crossOrigin="anonymous"
           />
-          {/* Global Site Tag (gtag.js) - Google Analytics */}
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-              page_path: window.location.pathname,
-            });
-          `,
-            }}
-          />
         </Head>
         <body>
+          <noscript>
+            <iframe
+              src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            />
+          </noscript>
           {/* Make Color mode to persists when you refresh the page. */}
           <ColorModeScript />
           <Main />
