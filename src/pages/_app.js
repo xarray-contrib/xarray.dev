6d212599c372542572f4e5e8f62cd6a25285f4f2
Anderson Banihirwe <axbanihirwe@ualr.edu>
2022-06-01 16:03:20 -0600

    Add Google Analytics (#217)

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,28 @@
 import { ChakraProvider } from "@chakra-ui/react"
+import { useEffect } from "react"
+import { useRouter } from "next/router"
 import theme from "../theme"
 import { MDXComponentsProvider } from "components/mdx"
+import * as ga from "../lib/ga"
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url)
+    }
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on("routeChangeComplete", handleRouteChange)
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <ChakraProvider resetCSS theme={theme}>
       <MDXComponentsProvider>
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -17,6 +17,23 @@ export default class Document extends NextDocument {
             type="text/css"
             crossOrigin="anonymous"
           />
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
         </Head>
         <body>
           {/* Make Color mode to persists when you refresh the page. */}
