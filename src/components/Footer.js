1e79e6cc54d9daa34b914e168173bf2babdec2c9
Anderson Banihirwe <axbanihirwe@ualr.edu>
2022-06-03 13:08:53 -0600

    add background color to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -50,7 +50,10 @@ const ListHeader = ({ children }) => {
 
 export const Footer = () => {
   return (
-    <Box>
+    <Box
+      bg={useColorModeValue("gray.50", "gray.900")}
+      color={useColorModeValue("gray.700", "gray.200")}
+    >
       <Container as={Stack} maxW={"6xl"} py={10}>
         <SimpleGrid
           templateColumns={{ sm: "1fr 1fr", md: "2fr 1fr 1fr 2fr" }}
