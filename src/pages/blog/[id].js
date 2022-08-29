f786614510b7c27112158e234875bc8f19695b29
Anderson Banihirwe <axbanihirwe@ualr.edu>
2022-08-29 15:49:21 -0600

    update blog

--- src/pages/blog/[id].js
+++ src/pages/blog/[id].js
@@ -2,6 +2,7 @@ import {
   Avatar,
   Box,
   Button,
+  Container,
   Divider,
   Flex,
   Heading,
@@ -41,67 +42,62 @@ export default function Post({ source, frontmatter, postId }) {
       description={frontmatter.summary}
       url={`https://xarray.dev/blog/${postId}`}
     >
-      <Box
-        py={10}
-        spacing={8}
-        justifyContent='center'
-        alignItems='flex-start'
-        m='0 auto 4rem auto'
-        maxWidth='4xl'
-      >
-        <Box spacing='3' alignItems='start'>
-          <VStack paddingTop='30px' spacing='2' alignItems='center'>
-            <Heading as={'h1'} textAlign={'center'} size='xl' my={4}>
-              {frontmatter.title}
-            </Heading>
-            <Text fontSize={'sm'} color={'gray.700'}>
-              {formatDate(date)} ({distanceToNow(date)})
-            </Text>
+      <Box as={'section'}>
+        <Container maxW='container.xl' py={10}>
+          <Box spacing='3' alignItems='start'>
+            <VStack paddingTop='30px' spacing='2' alignItems='center'>
+              <Heading as={'h1'} textAlign={'center'} size='xl' my={4}>
+                {frontmatter.title}
+              </Heading>
+              <Text fontSize={'sm'} color={'gray.700'}>
+                {formatDate(date)} ({distanceToNow(date)})
+              </Text>
 
-            <Wrap spacing='20px'>
-              {frontmatter.authors.map((author) => {
-                return (
-                  <WrapItem key={author.name}>
-                    <Flex
-                      as={Link}
-                      href={`https://github.com/${author.github}`}
-                      align={'center'}
-                      my={2}
-                      direction={'column'}
-                      _hover={{
-                        textDecoration: 'none',
-                      }}
-                    >
-                      <Avatar
-                        src={`https://github.com/${author.github}.png`}
-                        name={author.name}
+              <Wrap spacing='20px'>
+                {frontmatter.authors.map((author) => {
+                  return (
+                    <WrapItem key={author.name}>
+                      <Flex
+                        as={Link}
+                        href={`https://github.com/${author.github}`}
+                        align={'center'}
                         my={2}
-                      />
-                      <Text fontWeight={600}>{author.name}</Text>
-                    </Flex>
-                  </WrapItem>
-                )
-              })}
-            </Wrap>
-            <Divider my={2} />
-          </VStack>
-          <br></br>
-          <MDXRemote {...source} components={MDXElements} />
-        </Box>
+                        direction={'column'}
+                        _hover={{
+                          textDecoration: 'none',
+                        }}
+                      >
+                        <Avatar
+                          src={`https://github.com/${author.github}.png`}
+                          name={author.name}
+                          my={2}
+                        />
+                        <Text fontWeight={600}>{author.name}</Text>
+                      </Flex>
+                    </WrapItem>
+                  )
+                })}
+              </Wrap>
+              <Divider my={2} />
+            </VStack>
+            <br></br>
+            <MDXRemote {...source} components={MDXElements} />
+          </Box>
 
-        <Button
-          my={8}
-          as={Link}
-          href={'/blog'}
-          variant={'outline'}
-          leftIcon={<ArrowBackIcon />}
-          colorScheme={'blue'}
-        >
-          Back to Blog
-        </Button>
-        <Divider my={8} />
-        <br />
-        <Giscus />
+          <Button
+            my={8}
+            as={Link}
+            href={'/blog'}
+            variant={'outline'}
+            leftIcon={<ArrowBackIcon />}
+            colorScheme={'blue'}
+          >
+            Back to Blog
+          </Button>
+          <Divider my={8} />
+          <br />
+          <Giscus />
+        </Container>
       </Box>
     </Layout>
   )
