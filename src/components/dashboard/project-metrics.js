bd0762d3d7d2f9f9e63b3443d2d0850e1a10beac
Anderson Banihirwe <axbanihirwe@ualr.edu>
2022-08-29 15:38:43 -0600

    update dashboard

--- src/components/dashboard/project-metrics.js
+++ src/components/dashboard/project-metrics.js
@@ -2,7 +2,7 @@ import { DatasetteStatsCard } from '@/components/dashboard/datasette-stats-card'
 import { StatisticsCard } from '@/components/dashboard/statistics-card'
 import { Heading } from '@/components/mdx'
 import { fetcher } from '@/lib/data-fetching'
-import { SimpleGrid, Spinner } from '@chakra-ui/react'
+import { SimpleGrid, Spinner, Box, Container } from '@chakra-ui/react'
 import { BsPeople, BsPerson } from 'react-icons/bs'
 import { GoBook, GoPackage, GoStar, GoTag } from 'react-icons/go'
 import useSWR from 'swr'
@@ -32,60 +32,62 @@ export const ProjectMetrics = () => {
   const year = dateObj.getFullYear()
 
   return (
-    <>
-      {' '}
-      <Heading as='h1' size='2xl' my={4} textAlign={'center'}>
-        Xarray Project Metrics
-      </Heading>
-      <SimpleGrid columns={{ base: 1, md: 3 }} spacing={{ base: 5, lg: 8 }}>
-        <StatisticsCard
-          title={'Core Maintainers'}
-          stat={'15'}
-          icon={<BsPerson size={'3em'} />}
-          link={'https://docs.xarray.dev/en/stable/team.html'}
-        />
-        <DatasetteStatsCard
-          title={'Contributors'}
-          query={
-            'https://pydata-datasette.herokuapp.com/xarray/_analyze_tables_/contributors,user_id.json?_shape=array'
-          }
-          icon={<BsPeople size={'3em'} />}
-          link={'https://github.com/pydata/xarray/graphs/contributors'}
-        />
+    <Box as='section'>
+      <Container maxW='container.xl'>
+        {' '}
+        <Heading as='h1' size='2xl' textAlign={'center'}>
+          Xarray Project Metrics
+        </Heading>
+        <SimpleGrid columns={{ base: 1, md: 3 }} spacing={{ base: 5, lg: 8 }}>
+          <StatisticsCard
+            title={'Core Maintainers'}
+            stat={'15'}
+            icon={<BsPerson size={'3em'} />}
+            link={'https://docs.xarray.dev/en/stable/team.html'}
+          />
+          <DatasetteStatsCard
+            title={'Contributors'}
+            query={
+              'https://pydata-datasette.herokuapp.com/xarray/_analyze_tables_/contributors,user_id.json?_shape=array'
+            }
+            icon={<BsPeople size={'3em'} />}
+            link={'https://github.com/pydata/xarray/graphs/contributors'}
+          />
 
-        <DatasetteStatsCard
-          title={'Stargazers'}
-          icon={<GoStar size={'3em'} />}
-          query={
-            'https://pydata-datasette.herokuapp.com/xarray/_analyze_tables_/stars,user.json?_shape=array'
-          }
-          link={'https://github.com/pydata/xarray/stargazers'}
-        />
+          <DatasetteStatsCard
+            title={'Stargazers'}
+            icon={<GoStar size={'3em'} />}
+            query={
+              'https://pydata-datasette.herokuapp.com/xarray/_analyze_tables_/stars,user.json?_shape=array'
+            }
+            link={'https://github.com/pydata/xarray/stargazers'}
+          />
 
-        <DatasetteStatsCard
-          title={'Dependent Projects'}
-          query={
-            'https://pydata-datasette.herokuapp.com/xarray/_analyze_tables_/dependents,dependent.json?_shape=array'
-          }
-          icon={<GoPackage size={'3em'} />}
-          link={'https://github.com/pydata/xarray/network/dependents'}
-        />
+          <DatasetteStatsCard
+            title={'Dependent Projects'}
+            query={
+              'https://pydata-datasette.herokuapp.com/xarray/_analyze_tables_/dependents,dependent.json?_shape=array'
+            }
+            icon={<GoPackage size={'3em'} />}
+            link={'https://github.com/pydata/xarray/network/dependents'}
+          />
 
-        <StatisticsCard
-          title={`${month}/${year} Docs Visitors`}
-          stat={monthlyViews.users}
-          icon={<GoBook size={'3em'} />}
-        />
+          <StatisticsCard
+            title={`${month}/${year} Docs Visitors`}
+            stat={monthlyViews.users}
+            icon={<GoBook size={'3em'} />}
+          />
 
-        <DatasetteStatsCard
-          title={'Releases'}
-          query={
-            'https://pydata-datasette.herokuapp.com/xarray/_analyze_tables_/releases,id.json?_shape=array'
-          }
-          icon={<GoTag size={'3em'} />}
-          link={'https://github.com/pydata/xarray/releases'}
-        />
-      </SimpleGrid>
-    </>
+          <DatasetteStatsCard
+            title={'Releases'}
+            query={
+              'https://pydata-datasette.herokuapp.com/xarray/_analyze_tables_/releases,id.json?_shape=array'
+            }
+            icon={<GoTag size={'3em'} />}
+            link={'https://github.com/pydata/xarray/releases'}
+          />
+        </SimpleGrid>
+      </Container>
+    </Box>
   )
 }
