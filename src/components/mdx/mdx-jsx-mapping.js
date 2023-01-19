7a83473c1f3416b6800e3e701952ffbf3d4a332d
Anderson Banihirwe <13301940+andersy005@users.noreply.github.com>
2023-01-19 00:04:08 -0700

    Fix markdown (#429)

--- src/components/mdx/mdx-jsx-mapping.js
+++ src/components/mdx/mdx-jsx-mapping.js
@@ -1,21 +1,18 @@
-import { MDXProvider } from '@mdx-js/react'
-
 import { Code } from '@/components/mdx/code'
 import { Heading } from '@/components/mdx/heading'
 import { Hr } from '@/components/mdx/hr'
 import { Image } from '@/components/mdx/image'
 import { Link } from '@/components/mdx/link'
 import { Quote } from '@/components/mdx/quote'
-import { Box, Code as ChakraCode, Text } from '@chakra-ui/react'
+import { Box, Text } from '@chakra-ui/react'
 
-const mapping = {
+export const mapping = {
   h1: (props) => <Heading as='h1' size='2xl' my={4} {...props} />,
   h2: (props) => <Heading as='h2' size='lg' fontWeight='bold' {...props} />,
   h3: (props) => <Heading as='h3' size='md' fontWeight='bold' {...props} />,
   h4: (props) => <Heading as='h4' size='sm' fontWeight='bold' {...props} />,
   h5: (props) => <Heading as='h5' size='sm' fontWeight='bold' {...props} />,
   h6: (props) => <Heading as='h6' size='xs' fontWeight='bold' {...props} />,
-  inlineCode: (props) => <ChakraCode fontSize='sm' {...props} />,
   code: (props) => <Code {...props} />,
   br: (props) => <Box height='24px' {...props} />,
   hr: (props) => <Hr {...props} />,
@@ -26,12 +23,4 @@ const mapping = {
   li: (props) => <Box as='li' pb={1} {...props} />,
   blockquote: (props) => <Quote {...props} />,
   img: (props) => <Image {...props} alt={props.alt} />,
-}
-
-export const MDXComponentsProvider = (props) => {
-  return (
-    <MDXProvider components={mapping}>
-      <Box {...props} />
-    </MDXProvider>
-  )
 }
