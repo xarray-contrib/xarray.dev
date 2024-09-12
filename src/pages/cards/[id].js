0cfd2def0e71a060a20b425854113dfe4bd19ac0
Anderson Banihirwe <axbanihirwe@ualr.edu>
2024-09-11 17:33:04 -0700

    update opengraph images

--- src/pages/cards/[id].js
+++ src/pages/cards/[id].js
@@ -1,6 +1,4 @@
 import { Image } from '@/components/mdx'
-import { MdOutlineCalendarToday, MdPeopleOutline } from 'react-icons/md'
-
 import { formatDate } from '@/lib/date-formatting'
 import { getAllPostsIds, getPostData } from '@/lib/posts'
 import {
@@ -11,71 +9,73 @@ import {
   Flex,
   Heading,
   Icon,
-  Stack,
   Text,
+  VStack,
 } from '@chakra-ui/react'
 import fs from 'fs'
 import matter from 'gray-matter'
 import path from 'path'
+import { MdOutlineCalendarToday, MdPeopleOutline } from 'react-icons/md'
 
 const Card = ({ frontmatter, id }) => {
-  const boxBackground = 'teal.100'
   const date = new Date(frontmatter.date)
 
   return (
-    <Box
-      sx={{ bg: boxBackground, color: 'invert' }}
-      h={'100vh'}
-      w={'100vw'}
-      backgroundImage={'/background.svg'}
-    >
-      <Container id='post' maxW={'container.lg'} centerContent>
-        <Flex direction='column' fontSize={'2xl'}>
-          <Stack direction={'row'} spacing={8} justify='space-between'>
-            <Stack>
-              <Text my={32} fontWeight={'bold'} opacity={0.7}>
-                xarray.dev / blog
-              </Text>
-            </Stack>
+    <Box bg='teal.100' minH='100vh' w='100vw'>
+      <Container maxW='container.lg' h='100%' py={12}>
+        <Flex direction='column' h='100%' justify='center'>
+          <Flex justifyContent='space-between' alignItems='flex-start' mb={6}>
+            <Text fontSize='xl' color='gray.700' fontWeight='medium'>
+              xarray.dev / blog
+            </Text>
+            <Image
+              w='150px'
+              h='auto'
+              objectFit='contain'
+              src='/Xarray-assets/RGB/Xarray_Logo_RGB_Final.svg'
+              alt='xarray logo'
+            />
+          </Flex>
+
+          <VStack align='stretch' spacing={16} my={12}>
+            <Heading as='h1' size='3xl' fontWeight='bold' lineHeight='1.2'>
+              {frontmatter.title}
+            </Heading>
+
             <Flex
-              direction={'row'}
-              alignItems={'center'}
-              justify={'right'}
-              align={'right'}
+              direction={['column', 'row']}
+              justify='space-between'
+              align={['flex-start', 'center']}
+              wrap='wrap'
+              gap={4}
             >
-              <Image
-                justify={'right'}
-                align={'right'}
-                w={'200px'}
-                src={'/Xarray-assets/RGB/Xarray_Logo_RGB_Final.svg'}
-                alt={'xarray logo'}
-              />
+              <Flex align='center'>
+                <Icon
+                  as={MdOutlineCalendarToday}
+                  w={6}
+                  h={6}
+                  mr={2}
+                  color='gray.700'
+                />
+                <Text fontSize='lg' fontWeight='medium'>
+                  {formatDate(date)}
+                </Text>
+              </Flex>
             </Flex>
-          </Stack>
-          <Heading as={'h1'} size={'2xl'} my={8}>
-            {frontmatter.title}
-          </Heading>
 
-          <Stack direction={'row'} my={4} align={'center'}>
-            <Icon as={MdOutlineCalendarToday} w='8' h='8' />
-            <Text fontWeight={'bold'}>{formatDate(date)}</Text>
-          </Stack>
-
-          <Stack direction={'row'} my={8} align={'center'}>
-            <Icon as={MdPeopleOutline} w='8' h='8' />
-
-            <AvatarGroup size={'xl'}>
-              {frontmatter.authors.map((author) => {
-                return (
+            <Flex align='center'>
+              <Icon as={MdPeopleOutline} w={6} h={6} mr={2} color='gray.700' />
+              <AvatarGroup size='xl' max={5} spacing='-0.75rem'>
+                {frontmatter.authors.map((author) => (
                   <Avatar
                     key={author.name}
-                    src={`https://github.com/${author.github}.png`}
                     name={author.name}
+                    src={`https://github.com/${author.github}.png`}
                   />
-                )
-              })}
-            </AvatarGroup>
-          </Stack>
+                ))}
+              </AvatarGroup>
+            </Flex>
+          </VStack>
         </Flex>
       </Container>
     </Box>
