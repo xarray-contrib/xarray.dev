d40df96a201fb710f846f2da9fdd2156b9139b53
Gonzalo Peña-Castellanos <goanpeca@gmail.com>
2025-05-03 13:39:31 -0500

    Add local storage and fix language switcher

--- src/components/language-switcher.js
+++ src/components/language-switcher.js
@@ -1,19 +1,40 @@
+'use client'
 import { Select } from '@chakra-ui/react'
 import { useLingui } from '@lingui/react/macro'
-import { i18n } from "@lingui/core";
-import { useRouter } from 'next/navigation'
+import { i18n } from '@lingui/core'
+import { useRouter, usePathname } from 'next/navigation'
+import { useState, useEffect } from 'react'
 
 export const LanguageSwitcher = () => {
   const { t } = useLingui()
+  const router = useRouter()
+  const pathname = usePathname()
+  const [selectedValue, setSelectedValue] = useState('')
 
-  async function dynamicActivate(event) {
-    const locale = event.target.value
-    const catalog = await import(`../locales/${locale}/messages.js`)
-    i18n.load(locale, catalog.messages);
-    i18n.activate(locale);
+  async function changeLocale(event) {
+    const localeString = event.target.value
+    setSelectedValue(localeString)
+    localStorage.setItem('locale', localeString)
+    const catalog = await import(`../locales/${localeString}/messages.js`)
+    i18n.load(localeString, catalog.messages)
+    i18n.activate(localeString)
+    router.push(pathname, { locale: localeString })
   }
+
+  useEffect(() => {
+    const storedLocale = localStorage.getItem('locale')
+    if (storedLocale && storedLocale !== router.locale) {
+      changeLocale({ target: { value: storedLocale } })
+    }
+  }, [])
   return (
-    <Select variant='flushed' size='xs' defaultValue="en" onChange={dynamicActivate}>
+    <Select
+      name='selectedLocale'
+      variant='flushed'
+      size='xs'
+      value={selectedValue}
+      onChange={changeLocale}
+    >
       <option value='en'>{t`English`}</option>
       <option value='es'>{t`Spanish`}</option>
       <option value='pt'>{t`Portuguese`}</option>
